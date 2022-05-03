a5902e67b280b25e22a082d3d20de664097df801
swg <57892743+wegul@users.noreply.github.com>
2022-05-03 13:02:09 +0800

    read file

--- online/btonline.py
+++ online/btonline.py
@@ -11,152 +11,160 @@ import http.server
 import collections
 
 NUM_PRBS_PER_TTI = 50
-NUM_PRBS_START = 0.9*NUM_PRBS_PER_TTI
-NUM_PRBS_END = 0.4*NUM_PRBS_PER_TTI
+NUM_PRBS_START = 0.9 * NUM_PRBS_PER_TTI
+NUM_PRBS_END = 0.4 * NUM_PRBS_PER_TTI
+
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
-  def send_head(self):
-    if self.translate_path(self.path).endswith('/bw'):
-      body = "%0.3f" % bsegg
-      self.send_response(200)
-      self.send_header("Content-type", "text/html; charset=utf-8")
-      self.send_header("Content-Length", str(len(body)))
-      self.send_header("Access-Control-Allow-Origin", "*")
-      self.end_headers()
-      return io.StringIO(body)
-    else:
-      return super(MyHandler, self).send_head()
+    def send_head(self):
+        if self.translate_path(self.path).endswith('/bw'):
+            body = "%0.3f" % bsegg
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.send_header("Content-Length", str(len(body)))
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            return io.StringIO(body)
+        else:
+            return super(MyHandler, self).send_head()
+
 
 class PDSCHLog():
-  def __init__(self, jsonlog=""):
-    self.raw = jsonlog
-    try:
-      parsed = json.loads(self.raw) 
-      self.nRBs = sum([int(x) for x in bin(int(parsed["RB Allocation Slot 0[0]"], 0))[2:]])
-      self.fsf = int(parsed["System Frame Number"]) * 10 + int(parsed["Subframe Number"])
-      self.tbs0 = int(parsed["TBS 0"])
-      self.tbs1 = int(parsed["TBS 1"])
-      self.vol_dl = (self.tbs0 + self.tbs1) / 8.0 # bytes
-      self.time = (datetime.strptime(parsed["timestamp"], "%Y-%m-%d %H:%M:%S.%f") - datetime(1970, 1, 1)).total_seconds()
-    except:
-      self.nRBs = 2000
-      self.fsf = -1
-      self.tbs0 = -1
-      self.tbs1 = -1
-      self.vol_dl = -1
-      self.time = -1
+    def __init__(self, jsonlog=""):
+        self.raw = jsonlog
+        try:
+            parsed = json.loads(self.raw)
+            self.nRBs = sum([int(x) for x in bin(int(parsed["RB Allocation Slot 0[0]"], 0))[2:]])
+            self.fsf = int(parsed["System Frame Number"]) * 10 + int(parsed["Subframe Number"])
+            self.tbs0 = int(parsed["TBS 0"])
+            self.tbs1 = int(parsed["TBS 1"])
+            self.vol_dl = (self.tbs0 + self.tbs1) / 8.0  # bytes
+            self.time = (datetime.strptime(parsed["timestamp"], "%Y-%m-%d %H:%M:%S.%f") - datetime(1970, 1,
+                                                                                                   1)).total_seconds()
+        except:
+            self.nRBs = 2000
+            self.fsf = -1
+            self.tbs0 = -1
+            self.tbs1 = -1
+            self.vol_dl = -1
+            self.time = -1
 
-  def print_contents(self):
-    print("fsf %d, num PRBs = %d, DL vol = %0.2f Bytes\n" % (self.fsf, self.nRBs, self.vol_dl))
+    def print_contents(self):
+        print("fsf %d, num PRBs = %d, DL vol = %0.2f Bytes\n" % (self.fsf, self.nRBs, self.vol_dl))
+
 
 class BurstTracker():
-  def __init__(self):
-    self.in_burst = False
-    self.start_fsf = -1
-    self.end_fsf = -1
-    self.tdelta = -1
-    self.burst_volume = []
+    def __init__(self):
+        self.in_burst = False
+        self.start_fsf = -1
+        self.end_fsf = -1
+        self.tdelta = -1
+        self.burst_volume = []
 
-  def begin_burst(self, start_fsf):
-    self.in_burst = True
-    self.start_fsf = start_fsf
+    def begin_burst(self, start_fsf):
+        self.in_burst = True
+        self.start_fsf = start_fsf
 
-  def end_burst(self, end_fsf):
-    self.in_burst = False
-    self.end_fsf = end_fsf
+    def end_burst(self, end_fsf):
+        self.in_burst = False
+        self.end_fsf = end_fsf
 
-    # compute burst duration, accounting for SFN roll over
-    if self.end_fsf < self.start_fsf:
-      self.tdelta = 10249 - self.start_fsf + self.end_fsf # discount last TTI
-    else:
-      self.tdelta = self.end_fsf - self.start_fsf # discount last TTI
-    
-    if self.tdelta > 0:
-      return sum(self.burst_volume[:-1])/(self.tdelta) * 8e3 # bps
-    else:
-      return 0
+        # compute burst duration, accounting for SFN roll over
+        if self.end_fsf < self.start_fsf:
+            self.tdelta = 10249 - self.start_fsf + self.end_fsf  # discount last TTI
+        else:
+            self.tdelta = self.end_fsf - self.start_fsf  # discount last TTI
 
-  def add_volume(self, vol):
-    self.burst_volume.append(vol)
+        if self.tdelta > 0:
+            return sum(self.burst_volume[:-1]) / (self.tdelta) * 8e3  # bps
+        else:
+            return 0
 
-  def clear_burst(self):
-    self.in_burst = False
-    self.start_fsf = -1
-    self.end_fsf = -1
-    self.burst_volume = []
+    def add_volume(self, vol):
+        self.burst_volume.append(vol)
+
+    def clear_burst(self):
+        self.in_burst = False
+        self.start_fsf = -1
+        self.end_fsf = -1
+        self.burst_volume = []
+
 
 def setup_http_server():
-  HandlerClass = MyHandler
-  ServerClass  = http.server.HTTPServer
-  Protocol     = "HTTP/1.0"
+    HandlerClass = MyHandler
+    ServerClass = http.server.HTTPServer
+    Protocol = "HTTP/1.0"
 
-  port = 8000
-  server_address = ('127.0.0.1', port)
-  
-  HandlerClass.protocol_version = Protocol
-  httpd = ServerClass(server_address, HandlerClass)
+    port = 8000
+    server_address = ('127.0.0.1', port)
 
-  httpd.timeout = 0.1
-  return httpd
+    HandlerClass.protocol_version = Protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    httpd.timeout = 0.1
+    return httpd
+
 
 def serve_bandwidth(httpd):
-  httpd.handle_request()
-  
+    httpd.handle_request()
+
+
 def main():
-  global bsegg
-  bsegg = 0
-  # state variables
-  bt = BurstTracker()
-  plog_prev = PDSCHLog()
-  burst_hist = collections.deque(maxlen=5)
-  dur_hist = collections.deque(maxlen=5)
+    global bsegg
+    bsegg = 0
+    # state variables
+    bt = BurstTracker()
+    plog_prev = PDSCHLog()
+    burst_hist = collections.deque(maxlen=5)
+    dur_hist = collections.deque(maxlen=5)
 
-  # Setup HTTP server
-  httpd = setup_http_server()
+    # Setup HTTP server
+    httpd = setup_http_server()
 
-  print ("Starting BurstTracker...")
- 
-  while True:
-    ready = select([sys.stdin, httpd], [], [], 0.1)[0]
+    print("Starting BurstTracker...")
 
+    # while True:
+    #   ready = select([sys.stdin, httpd], [], [], 0.1)[0]
+    ready = "filename"
     for file in ready:
-      if file == sys.stdin:
-        line = sys.stdin.readline()
-        if line and "Subframe" in line: 
-    
-          ### parse current PDSCH log ###
-          plog_curr = PDSCHLog(line[45:]) # trim out "[INFO] [MsgLogger]: " suffix
-    
-          ### account for SFN roll over ###
-          if plog_curr.fsf < plog_prev.fsf:
-            tdelta = 10249 - plog_prev.fsf + plog_curr.fsf
-          else:
-            tdelta = plog_curr.fsf - plog_prev.fsf
-    
-          ### burst IN/OUT logic ###
-          if bt.in_burst and plog_prev.nRBs < NUM_PRBS_END and tdelta > 1: 
-            # END burst if: (1) prev TTI satisfies end threshold, (2) next TTI is empty
-            bthp = bt.end_burst(plog_prev.fsf)
-            burst_hist.append(bthp)
-            dur_hist.append(bt.tdelta)
-            bsegg = sum([dur_hist[i] * burst_hist[i] for i in range(len(burst_hist))]) / sum(dur_hist)
-            print ("%0.3f BURST from (%d, %d) with throughput %0.3f Mbps (avg %0.3f)" % (plog_prev.time, bt.start_fsf, bt.end_fsf, bthp/1e6, bsegg/1e6))
-            bt.clear_burst()
-          if not bt.in_burst and plog_curr.nRBs > NUM_PRBS_START:
-            # BEGIN burst if: TTI satisfies start threshold
-            bt.begin_burst(plog_curr.fsf)
-          if bt.in_burst:
-            # account for volume from every TTI during burst
-            bt.add_volume(plog_curr.vol_dl)
-    
-          ### save previous state ###
-          plog_prev = plog_curr
-        elif not len(line):
-          print ("detected EOF")
-          exit()
-      elif file == httpd:
-        httpd.handle_request()
+        if file == sys.stdin:
+            line = sys.stdin.readline()
+            if line and "Subframe" in line:
+
+                ### parse current PDSCH log ###
+                plog_curr = PDSCHLog(line[45:])  # trim out "[INFO] [MsgLogger]: " suffix
+
+                ### account for SFN roll over ###
+                if plog_curr.fsf < plog_prev.fsf:
+                    tdelta = 10249 - plog_prev.fsf + plog_curr.fsf
+                else:
+                    tdelta = plog_curr.fsf - plog_prev.fsf
+
+                ### burst IN/OUT logic ###
+                if bt.in_burst and plog_prev.nRBs < NUM_PRBS_END and tdelta > 1:
+                    # END burst if: (1) prev TTI satisfies end threshold, (2) next TTI is empty
+                    bthp = bt.end_burst(plog_prev.fsf)
+                    burst_hist.append(bthp)
+                    dur_hist.append(bt.tdelta)
+                    bsegg = sum([dur_hist[i] * burst_hist[i] for i in range(len(burst_hist))]) / sum(dur_hist)
+                    print("%0.3f BURST from (%d, %d) with throughput %0.3f Mbps (avg %0.3f)" % (
+                    plog_prev.time, bt.start_fsf, bt.end_fsf, bthp / 1e6, bsegg / 1e6))
+                    bt.clear_burst()
+                if not bt.in_burst and plog_curr.nRBs > NUM_PRBS_START:
+                    # BEGIN burst if: TTI satisfies start threshold
+                    bt.begin_burst(plog_curr.fsf)
+                if bt.in_burst:
+                    # account for volume from every TTI during burst
+                    bt.add_volume(plog_curr.vol_dl)
+
+                ### save previous state ###
+                plog_prev = plog_curr
+            elif not len(line):
+                print("detected EOF")
+                exit()
+        elif file == httpd:
+            httpd.handle_request()
+
 
 if __name__ == "__main__":
-  main()
-
+    main()
